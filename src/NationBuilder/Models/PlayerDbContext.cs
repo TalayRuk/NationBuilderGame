72b8176cd9ce34e20ca310688e42d29c7865d127
rouz majlessi <rmajlessi@gmail.com>
2016-12-06 17:00:34 -0800

    initial add

---
+++ src/NationBuilder/Models/PlayerDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NationBuilder.Models
+{
+    public class PlayerDbContext : IdentityDbContext<Player>
+    {
+        public PlayerDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+    }
+}
